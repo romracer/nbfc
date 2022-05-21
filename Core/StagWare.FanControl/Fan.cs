8b31bf4173a1d09cb9d94d698d45baf01aab95f5
emptyset0 <16044351+emptyset0@users.noreply.github.com>
2022-05-21 10:51:46 +0900

    Lower CriticalTemperatureOffset from 15 to 8

--- Core/StagWare.FanControl/Fan.cs
+++ Core/StagWare.FanControl/Fan.cs
@@ -10,7 +10,7 @@ namespace StagWare.FanControl
         #region Constants
 
         public const int AutoFanSpeed = 101;
-        private const int CriticalTemperatureOffset = 15;
+        private const int CriticalTemperatureOffset = 8;
 
         #endregion
 
